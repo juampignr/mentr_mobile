c339cc2c4e79ea1214f84e8f9c770f9272ff7582
juampignr <juampi.gnr@gmail.com>
2025-06-06 18:27:53 -0300

    .

---
+++ components/Spinner.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import Animated, {
+  useSharedValue,
+  withRepeat,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from "react-native-reanimated";
+import AntDesign from "react-native-vector-icons/AntDesign";
+
+export default function Spinner({ size = 60, color = "#b147ff99" }) {
+  const rotation = useSharedValue(0);
+
+  useEffect(() => {
+    rotation.value = withRepeat(
+      withTiming(360, { duration: 1000, easing: Easing.linear }),
+      -1,
+      false,
+    );
+  }, [rotation]);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ rotate: `${rotation.value}deg` }],
+    };
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.View style={animatedStyle}>
+        <AntDesign name="loading2" size={size} color={color} />
+      </Animated.View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+});
